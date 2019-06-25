e52277bbb5f9d1042e7bf25f3ea3bf782e44f669
Nicky_Dover.exe <dovern42@gmail.com>
2019-06-25 13:14:23 -0400

    Update pleaseclap.rb

--- pleaseclap.rb
+++ pleaseclap.rb
@@ -3,8 +3,7 @@ require 'net/http'
 require 'json'
 
 # This is a single file command line applet that will take your user data and clap for you 50 times on Medium. 
-# It was built so my students could support each other's blogs while seeing a demonstration of the power of the Chrome Network Tab.
-# I originally had this code just have them enter their variables in a cloned copy of the code but decided to have fun and make it a CLI app.
+# I originally had this code just have users enter their variables in a cloned copy of the code but decided to have fun and make it a CLI app.
 
 #####INTRO######
 puts 'Welcome to PLEASE CLAP, an applet that allows you to clap 50 times for Medium pieces in one go.'
